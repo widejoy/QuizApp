d063e2ebb413ebc4b578d5a7e9f96f5cd38ebf28
widejoy <rogerantony40@ug.cusat.ac.in>
2023-05-11 22:40:44 +0530

    updated the design

--- lib/questions.dart
+++ lib/questions.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/models/data.dart';
 
 void fun() {}
 
 class QustionScreen extends StatefulWidget {
+
   const QustionScreen({super.key});
 
   @override
@@ -13,33 +15,36 @@ class QustionScreen extends StatefulWidget {
 }
 
 class _QuestionScreen extends State<QustionScreen> {
+  final currentQuestion = qustions[0];
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children:  [
-          const Text('ad'),
+          Text(currentQuestion.text,
+          style: const TextStyle(color: Colors.white),
+          ),
           const SizedBox(
-            height: 10,
+            height: 30,
             width: 10,
           ),
-          ButtomStyle('hello',fun),
-          const SizedBox(
-            height: 15,
-            width: 10,
-          ),
-          ButtomStyle('hello',fun),
+          ButtomStyle(currentQuestion.answers[0],fun),
           const SizedBox(
             height: 15,
             width: 10,
           ),
-          ButtomStyle('hello',fun),
+          ButtomStyle(currentQuestion.answers[1],fun),
           const SizedBox(
             height: 15,
             width: 10,
           ),
-          ButtomStyle('hello',fun)
+          ButtomStyle(currentQuestion.answers[2],fun),
+          const SizedBox(
+            height: 15,
+            width: 10,
+          ),
+          ButtomStyle(currentQuestion.answers[3],fun)
         ],
       ),
     );
