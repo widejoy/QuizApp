cf8b08f6fac8caa264b5d6d2871893559be25338
widejoy <rogerantony40@ug.cusat.ac.in>
2023-05-12 22:11:52 +0530

    layout done for result screen

--- lib/results.dart
+++ lib/results.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
-class Results extends StatelessWidget{
+class Results extends StatelessWidget {
   const Results({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:  [
+          const Text(
+            'answer',
+            style: TextStyle(
+              color: Color.fromARGB(255, 210, 102, 237),
+            ),
+          ),
+          const SizedBox(height: 20,),
+          const Text('list of answers and questions'),
+          TextButton(onPressed: () {}, child: const Text('restart quiz'))
+        ],
+      ),
+    );
   }
-  
 }
